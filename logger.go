2911b124e270ee3c76752525f7641454f9ad651a
Xuyuan Pang <xuyuanp@gmail.com>
2014-08-09 01:01:52 +0800

    remove LevelFilter

--- logger.go
+++ logger.go
@@ -45,11 +45,14 @@ func NewLogger(name string, level LogLevel) *Logger {
 		Name:  name,
 		Level: level,
 	}
-	l.AddFilter(NewLevelFilter(level))
+	registerLogger(l)
 	return l
 }
 
 func (l *Logger) log(level LogLevel, msg string) {
+	if level < l.Level {
+		return
+	}
 	now := time.Now()
 	_, file, line, ok := runtime.Caller(2)
 	if !ok {
